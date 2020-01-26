2cf76031c4dfbbf2468da626de3b05c9049a8f22
Priyobrato Das <priyodas12@gmail.com>
2020-01-26 20:18:17 +0530

    added comment

--- Hackerrank/src/io/priyo_workspace/string_programs/DulicateCharRemoval.java
+++ Hackerrank/src/io/priyo_workspace/string_programs/DulicateCharRemoval.java
@@ -14,6 +14,7 @@ public class DulicateCharRemoval {
     static int alternatingCharacters(String s) {
     	
     	int result=0;
+    	//count from 1
     	for (int i = 0; i < (s.length() - 1); i++) {
             if (s.charAt(i) == s.charAt(i + 1)) 
                 result++;    
