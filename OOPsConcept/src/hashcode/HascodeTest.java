9ba662c89e01257f98de4b8d944631c265b7f39e
Priyobrato Das <priyodas12@gmail.com>
2020-03-21 08:45:37 +0530

    new hashcode implementation committed

---
+++ OOPsConcept/src/hashcode/HascodeTest.java
@@ -0,0 +1,21 @@
+package hashcode;
+
+public class HascodeTest {
+
+	public static void main(String[] args) {
+		Instance i1=new Instance();
+		Instance i2=new Instance();
+		System.out.println(i1.hashCode()+","+i2.hashCode());
+
+	}
+
+}
+class Instance {
+	
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+	
+}
