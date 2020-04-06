3a625e30232e36f9f0eb9be490f7e9a1810bf433
Priyobrato Das <priyodas12@gmail.com>
2020-04-06 17:12:11 +0530

    committed substring

--- OOPsConcept/src/hashcode/HascodeTest.java
+++ OOPsConcept/src/hashcode/HascodeTest.java
@@ -1,7 +1,9 @@
 package hashcode;
 
+/*This is typically implemented by converting the internal address of the object into an integer, 
+ * but this implementation technique is not required by the JavaTM programming language*/
 public class HascodeTest {
-
+	
 	public static void main(String[] args) {
 		Instance i1=new Instance();
 		Instance i2=new Instance();
