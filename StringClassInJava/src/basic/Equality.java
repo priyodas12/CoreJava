1e48dabac95f4b16ef7117d709e7aa9aa4d2f350
Priyobrato Das <priyodas12@gmail.com>
2020-04-22 17:52:41 +0530

    added string buffer classes

--- StringClassInJava/src/basic/Equality.java
+++ StringClassInJava/src/basic/Equality.java
@@ -14,10 +14,14 @@ public class Equality {
 		String s4=new String("abc");
 		String s6=new String("a");
 		String s7=s6+"bc";
+		final String s8=s6+"bc";
+		final String s9="a"+"bc";
 		//reference will be compared.
 		System.out.println(s1==s2?"s1==s2":"s1!=s2");
 		System.out.println(s1==s5?"s1==s5":"s1!=s5");
 		System.out.println(s1==s7?"s1==s7":"s1!=s7");
+		System.out.println(s1==s8?"s1==s8":"s1!=s8");
+		System.out.println(s1==s9?"s1==s9":"s1!=s9");
 		System.out.println(s1==s3?"s1==s3":"s1!=s3");
 		System.out.println(s3==s4?"s3==s4":"s3!=s4");
 		//String class overrides equals method and by default it will check content 
