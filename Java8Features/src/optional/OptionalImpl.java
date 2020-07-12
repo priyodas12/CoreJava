e3379c4b43370a6ee895505e6e68561cabb8db74
Priyobrato Das <priyodas12@gmail.com>
2020-07-12 19:49:20 +0530

    optional

--- Java8Features/src/optional/OptionalImpl.java
+++ Java8Features/src/optional/OptionalImpl.java
@@ -2,6 +2,10 @@ package optional;
 
 import java.util.Optional;
 
+/*Optional was introduced to Java as a way to fix the issues with null references.
+ Before Optional, every object was allowed to either contain a value or not (i.e. being null).
+ */
+
 public class OptionalImpl {
     public static void main(String[] args) {
         String s=null;
@@ -10,5 +14,11 @@ public class OptionalImpl {
         Optional<String> opsString=Optional.ofNullable(s);
         //ofNullable method will return Optional.empty for null pointed object
         System.out.println(opsString);
+        //to create empty optional object
+        Optional<String> opsString2=Optional.empty();
+        System.out.println(opsString2);
+        //
+        Optional<String> optionalFalcon = Optional.ofNullable("Priyo");
+        optionalFalcon.ifPresent(System.out::println);
     }
 }
